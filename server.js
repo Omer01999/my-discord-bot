bca23aad7258a07bdb0b2fc085ddb4c882c66137
Omer01999 <omertiktok88@gmail.com>
2025-03-02 03:13:54 +0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config(); // .env dosyasını yükle
+const { Client, GatewayIntentBits } = require('discord.js');
+
+// Yeni bir bot istemcisi oluştur
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+// Bot başlatıldığında çalışacak kod
+client.once('ready', () => {
+  console.log(`Bot ${client.user.tag} olarak giriş yaptı!`);
+});
+
+// /ip komutunu dinleme
+client.on('messageCreate', (message) => {
+  if (message.content === '/ip') {
+    message.reply('Server Yaxın Bir Vaxtda Açılacaqdır');
+  }
+});
+
+// Bot token'ı ile giriş yap
+client.login(process.env.BOT_TOKEN);
